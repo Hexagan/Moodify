3492edca11380738ff98871cd73af3167910a6c4
Federico <38073413+Hexagan@users.noreply.github.com>
2026-06-21 04:28:04 -0300

    vercel en CORS

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://moodify-one-rho.vercel.app/"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
