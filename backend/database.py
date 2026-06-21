e86180826c2e5cb65e78645b6ab3dabe894f14bc
Federico <38073413+Hexagan@users.noreply.github.com>
2026-06-21 10:51:02 -0300

    Database URL para hostear en Railway

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost:5432/moodify"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:1234@localhost:5432/moodify")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
